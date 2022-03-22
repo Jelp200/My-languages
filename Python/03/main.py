5af4c5976353a9db0390286dd3998cc266fd72a8
Jelp200 <jorge.pena0516@gmail.com>
2022-03-21 22:34:20 -0600

    :snake: [ADD] New blocks

--- Python/03/main.py
+++ Python/03/main.py
@@ -57,4 +57,51 @@ else:
     print("Give a correct value...")
 
 print(f"The number that you give is {n} and in text is {nText}")
+
+# Ternary operator
+condition = False
+print("True condition") if condition else print("False condition")
+
+# Season
+month = int(input("Give a month of the year (1-12): "))
+season = None
+
+if month == 1 or month == 2 or month == 12:
+    season = "Winter"
+elif month == 3 or month == 4 or month == 5:
+    season = "Spring"
+elif month == 6 or month == 7 or month == 8:
+    season = "Summer"
+elif month == 9 or month == 10 or month == 11:
+    season = "Fall"
+else:
+    season = "Incorrect month"
+print(f"For the month {month} the season is {season}")
+
+# Life
+age = int(input("Give your age: "))
+
+if age <= 12:
+    print("0 - 12: Child")
+elif (age > 13) and (age <= 19):
+    print("13 - 19: Teen")
+elif (age > 20) and (age <= 30):
+    print("20 - 30: Young adult")
+else:
+    print("Other age")
 """
+# Notes
+note = float(input("Give the note (0 - 10): "))
+
+if (note >= 9) or (note == 10):
+    print("A")
+elif (note >= 8) or (note == 9):
+    print("B")
+elif (note >= 7) or (note == 8):
+    print("C")
+elif (note >= 6) or (note == 7):
+    print("D")
+elif note <= 6:
+    print("F")
+else:
+    print("Give a correct note")
